44358bf2550a9fc42dc9c6180cefae9709e99f48
unknown <yosialukumai@gmail.com>
2022-07-13 13:29:50 +0300

    added few of the connection

--- src/Main.py
+++ src/Main.py
@@ -3,19 +3,41 @@ from pymongo import MongoClient
 
 
 class PyMongoWrappers:
+    """
+    PyMongoWrappers like Mongoose enables someone to fast create, Model(Collections)
+    Use it to Search and navigate on the given cluster
+    """
     url = ""
-    def __init__(self, url: str):
-        self.client = MongoClient(url)
-        print('initiated...')
+    def __init__(self, url=None):
+        if url == None:
+            # create the default from mongoclient
+            self.connection = MongoClient()
+        else:
+            self.connection = MongoClient(url)
+
+    def createModel(self, data:dict):
+        """
+        The method is very helpful in creating the model structure that one will be using
+        To create a  colllection
+        """
         pass
 
     def createCollection(self, data:dict):
         print('created the collection')
         pass
 
+    def closeConnection(self, ):
+        """
+        The method will be closing the connection with the database disconnecting the server
+        """
+    def testDatabase(self, ):
+        return self.connection['test_database']
+
     def searchData(self, data):
         pass
 
 
 
-myPY = PyMongoWrappers('mongodb://localhost:27017/')
+connection = PyMongoWrappers()
+
+print(type(connection.testDatabase()))
